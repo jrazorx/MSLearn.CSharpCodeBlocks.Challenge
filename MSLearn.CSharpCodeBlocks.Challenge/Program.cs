a15c5cc69f287195ef0ec58c6cc0958bce1f87fb
jra <julien.gault.50@gmail.com>
2024-02-26 16:22:48 +0100

    Add project files.

---
+++ MSLearn.CSharpCodeBlocks.Challenge/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] numbers = { 4, 8, 15, 16, 23, 42 };
+
+foreach (int number in numbers)
+{
+    int total;
+
+    total += number;
+
+    if (number == 42)
+    {
+        bool found = true;
+
+    }
+
+}
+
+if (found)
+{
+    Console.WriteLine("Set contains 42");
+
+}
+
+Console.WriteLine($"Total: {total}");
